61156a4a8c091fe58564a68d428f0ac892a1bc5e
Brody Rethy <brody@rethy.xyz>
2020-12-17 12:59:32 -0400

    files will be moved to own repo when complete

---
+++ viewer.py
@@ -0,0 +1,43 @@
+import os, time, paramiko
+
+#uses ssh key
+
+#connect to server
+#look for ~/file1, if exists, create ~/file2 and use as $userfile, etc... file1 is now $theirfile
+#look for ~/file1, if exists, look for file2, if exists, look for file3, etc.
+#subroutine is launched, which connects via ssh and monitors $their file for changes... 
+    #each time there is a change, it is captured and appended to $their chat log ~/.local/share/ssh-chat/their.log and overwrites ~/.local/share/ssh-chat/all_history.log
+#message is sent over SSH and overwrites $userfile
+
+#TODO
+#if no ssh key, exit
+
+def main():
+    if (check_for_config == False):
+        chat_serv_ip_addr = input("Server IP address: ")
+        chat_serv_username = input("Server SSH user: ")
+        make_configs(chat_serv_ip_addr, chat_serv_username)
+
+    their_file = ""
+    while True:
+        ssh = paramiko.SSHClient()
+        k = paramiko.RSAKey.from_private_key_file("~/.ssh/")
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(chat_serv_ip_addr, username=chat_serv_username, pkey=k)
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
+        
+        if (stdin != their_file):
+            print(file_contents)
+        time.sleep(1)
+
+def check_for_config():
+    if (os.path.isdir("~/.local/share/ssh_chat") and os.path.exists("~/.local/share/ssh_chat/chat_serv/chat_serv_ip_addr") and os.path.exists("~/.local/share/ssh_chat/chat_serv/chat_serv_username")):
+        return true
+    else:
+        return false
+
+def make_configs(chat_serv_ip_addr, chat_serv_username):
+    os.system("echo '" + chat_serv_ip_addr + "' > ~/.local/share/chat_serv/chat_serv_ip_addr")
+    os.system("echo '" + chat_serv_username + "' > ~/.local/share/chat_serv/chat_serv_username")
+
+main()
