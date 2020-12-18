b2b5287463a767467de6be3cd84f342a6784f61d
Brody Rethy <brody@rethy.xyz>
2020-12-18 18:11:33 -0400

    editing 4chan_media_scraper script

--- 4chan_media_scraper.py
+++ 4chan_media_scraper.py
@@ -1,5 +1,10 @@
-import re, requests, time
+import re, requests, time, os
 from requests_html import HTMLSession
+
+def write_photo_number(photo_counter):
+    f = open("photo_counter.txt", "w")
+    f.write(str(photo_counter))
+    f.close()
 
 def get_links(user_input):
     session = HTMLSession()
@@ -8,14 +13,13 @@ def get_links(user_input):
     return links
 
 def download_content(file_extension, links):
-    counter = 0
     try:
         x = re.compile(".*" + file_extension)
         linkSearch = list(filter(x.match, links))
         for link in linkSearch:
-            counter = counter + 1
+            photo_counter = photo_counter + 1
             req = requests.get('http:' + link)
-            with open(str(counter) + "." + file_extension, 'wb') as f:
+            with open(str(photo_counter) + "." + file_extension, 'wb') as f:
                 f.write(req.content)
     except:
         print("Either the URL doesn't exist, or you aren't connected to the Internet")
@@ -28,11 +32,17 @@ def main():
         #"webm"
     file_extensions = ["png", "gif", "jpg", "jpeg"]
 
+    if (os.path.isfile("photo_counter.txt") == False):
+        photo_counter = 0
+    else:
+        fo = open("photo_counter.txt", "r")
+        photo_counter = fo.read()
+
     while (True):
         user_input = input("Insert the URL of the 4chan thread you'd like to download from (Press Return to quit): ")
-        if (user_input == ""):
+        if (not user_input):
+            write_photo_number(photo_counter)
             print("Ensure that you have removed or renamed your downloaded images to avoid accidental overwrites")
-            time.sleep(5)
             quit()
 
         links = get_links(user_input)
