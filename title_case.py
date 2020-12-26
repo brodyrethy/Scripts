dd81ee217b94b7be5679507b87519c3d07f62732
Brody Rethy <brody@rethy.xyz>
2020-12-25 21:43:29 -0400

    added a loop

--- title_case.py
+++ title_case.py
@@ -2,19 +2,30 @@
 import readline
 
 def main():
-    words = input("Insert title: ")
-    words = words.split(" ")
+    counter = 0
 
-    # separator
-    print("")
+    while (True):
+        words = input("Insert title (press Return to quit): ")
+        if (not words):
+            quit()
+        words = words.split(" ")
 
-    for x in words:
-        word = x.lower()
-        if ((word == "a") or (word == "an") or (word == "the") or (word == "in") or (word == "on") or (word == "by") or (word == "with") or (word == "of") or (word == "and") or (word == "but") or(word == "or")):
-            print(word.lower(), end=" ")
-        else:
-            print(word.capitalize(), end=" ")
+        # separator
+        print("")
 
-    input("\n\nPress Enter to close")
+        for x in words:
+            counter += 1
+
+            word = x.lower()
+
+            if (counter <= 1):
+                print(word.capitalize(), end=" ")
+            elif ((word == "a") or (word == "an") or (word == "the") or (word == "in") or (word == "on") or (word == "by") or (word == "with") or (word == "of") or (word == "and") or (word == "but") or(word == "or")):
+                print(word.lower(), end=" ")
+            else:
+                print(word.capitalize(), end=" ")
+
+        print("\n")
+
 
 main()
