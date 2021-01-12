542eab0ab69f335381289864082cf94bcc5a9b53
brody <brody@thewired-desktop.xyz>
2021-01-12 18:42:09 -0400

    randomize_wallpaper.py now takes args as system distinguisher

--- randomize_wallpaper.py
+++ randomize_wallpaper.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
-import os, random, getpass
+import os, random, getpass, sys
 
 def main():
-    username = get_system_username()
     all_files = []
+
+    device = sys.argv
+
+    if (len(device) < 2):
+        quit(":: insert your device")
+
+    device.pop(0) # remove the filename
+    device = device[0]
+
+    username = get_system_username()
 
     paths = ["architecture", "painting", "city", "computer", "fractals", "game", "landscape", "music", "psy", "space", "math", "orthodoxy"]
 
     sub_folder = random.choice(paths)
 
-    path = "/home/" + username + "/1TBDrive/pictures/wallpapers/" + sub_folder + "/"
+    if (device == "laptop"):
+        path = "/home/" + username + "/1TBDrive/pictures/wallpapers/" + sub_folder + "/"
+    elif (device == "desktop"):
+        path = "/home/" + username + "/500GigDrive1/pictures/wallpapers/" + sub_folder + "/"
+    else:
+        quit(":: insert your device")
     
     for x in files(path):
         all_files.append(x)
