a04a7728c33c422c970bb6eb3b651b3ad52d62c6
Brody Rethy <brody@rethy.xyz>
2021-02-17 08:54:45 -0400

    renamed some scripts

--- dwmbar_desktop.sh
+++ dwmbar_desktop.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
 
 print_mpd_time() {
-	mpc_time=$(mpc -p 6601 | sed -n 2p | awk '{print $3}')
-	echo -e "$mpc_time"
+	mpcTime=$(mpc -p 6601 | sed -n 2p | awk '{print $3}')
+	echo -e "$mpcTime"
 }
 
 print_mpd() {
-	mpc_current=$(mpc -p 6601 current)
-	echo -e "$mpc_current"
+	mpcCurrent=$(mpc -p 6601 current)
+	echo -e "$mpcCurrent"
 }
 
 print_volume() {
 	volume=$(echo $(pulsemixer --get-volume | awk '{print $2}')%)
-	echo -e "V:$volume"
+	echo -e "$volume"
 }
 
 print_wifi() {
 	ip=$(ip addr|awk '/enp2s0/ && /inet/ {gsub(/\/[0-9][0-9]/,""); print $2}')
-	echo -e "IP:$ip"
+	echo -e "$ip"
 }
 
 print_mem() {
-	mem_free=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
-	mem_sym=$(echo "MB")
-	echo -e "M:$mem_free$mem_sym"
+	memFree=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
+	memSym=$(echo "MB")
+	echo -e "$memFree$memSym"
 }
 
 print_date(){
-	date=$(date "+%a %m-%d %r")
-	echo -e "D:$date"
+	date=$(date +"%m-%d")
+	time=$(date +"%R")
+	echo -e "$date |$time"
 }
 
-show_record(){
-	test -f /tmp/r2d2 || return
-	rp=$(cat /tmp/r2d2 | awk '{print $2}')
-	size=$(du -h $rp | awk '{print $1}')
-	echo " $size $(basename $rp)"
+separator(){
+	sep=$(echo " |")
+	echo -e "$sep"
 }
 
 while true
 do
-	xsetroot -name "$(print_mpd_time) $(print_mpd) $(print_volume) $(print_mem) $(print_date)"
+
+	xsetroot -name "$(print_mpd_time) $(print_mpd)$(separator)$(print_volume)$(separator)$(print_mem)$(separator)$(print_wifi)$(separator)$(print_date)"
 
 	sleep 1
+
 done
