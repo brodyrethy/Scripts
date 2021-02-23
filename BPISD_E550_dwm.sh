c991ff74ae66c38ce807daeb12381509e5a66cb3
Brody Rethy <brody@rethy.xyz>
2021-02-23 11:56:16 -0400

    finally updated BPISD_E550_dwm install script

--- BPISD_E550_dwm.sh
+++ BPISD_E550_dwm.sh
@@ -1,116 +1,106 @@
 #!/bin/bash
+git clone https://github.com/rethyxyz/dotfiles ~/dotfiles
 
-get_dotfiles() {
-    git clone https://github.com/brodyrethy/dotfiles ~/dotfiles
-	wget "https://github.com/haikarainen/light/releases/download/v1.2/light_1.2_amd64.deb"
-}
 
-apply_ownership() {
-    sudo chown $USER:$USER -R ~
-}
 
-mk_dirs() {
-    sudo mkdir -p ~/.fonts
-    sudo mkdir -p ~/1TBDrive
-    sudo mkdir -p ~/.config/mpc
-	sudo mkdir -p ~/.vim/undodir
-	sudo mkdir -p ~/.Trash
-	sudo mkdir -p /mnt/thewired_server
-}
+# Set proper permissions
+sudo chown $USER:wheel -R ~
 
-mv_files_and_dirs() {
-	#THIS COMMAND MUST BE RUN FIRST TO GET config.h
-	sudo cp ~/dotfiles/.config/config.h_e550 ~/dotfiles/.config/dwm/config.h
 
-	sudo cp -R ~/dotfiles/.fonts ~/
-    sudo cp -R ~/dotfiles/.config/dwm ~/.config/
-    sudo cp -R ~/dotfiles/.config/mpd ~/.config
-    sudo cp -R ~/dotfiles/.config/ranger ~/.config
-    sudo cp -R ~/dotfiles/.config/ncmpcpp ~/.config
-    sudo cp -R ~/dotfiles/.config/st ~/.config/
 
-    sudo cp ~/dotfiles/inputrc /etc/
-    sudo cp ~/dotfiles/grub /etc/default/
-    sudo cp ~/dotfiles/sources.list /etc/apt/
-    sudo cp ~/dotfiles/nobeep.conf /etc/modprobe.d/
-    sudo cp ~/dotfiles/pulseaudio.service /etc/systemd/system/
+# Mkdirs
+sudo mkdir -p ~/.fonts
+sudo mkdir -p ~/1TBDrive
+sudo mkdir -p ~/.vim/undodir
+sudo mkdir -p ~/.Trash/files
+sudo mkdir -p /mnt/
 
-    sudo cp ~/dotfiles/.xinitrc ~/
-    sudo cp ~/dotfiles/.Xdefaults ~/
-    sudo cp ~/dotfiles/.bash_aliases ~/
-    sudo cp ~/dotfiles/.bash_profile ~/
-    sudo cp ~/dotfiles/.vimrc ~/
-    sudo cp ~/dotfiles/.bashrc ~/
-}
 
-echo_data() {
-	#.xinitrc
-	echo "xinput disable 'AlpsPS/2 ALPS DualPoint TouchPad'" >> ~/.xinitrc
-	echo "~/Scripts/dwmbar_e550.sh &" >> ~/.xinitrc
-	echo "" >> ~/.xinitrc
-	echo "exec dwm" >> ~/.xinitrc
-}
 
-get_programs() {
-    sudo apt update -y
-    sudo apt install ranger firmware-iwlwifi network-manager feh xinit vim lxappearance x11-xserver-utils pulseaudio curl mpd mpc ncmpcpp firefox-esr xinput python3-pip mpv imagemagick irssi calcurse newsboat fuse cifs-utils zathura zathura-cb zathura-pdf-poppler gparted libax25-dev libukwm-1-dev python3-pip mercurial python-dev python3-dev ruby ruby-dev libx11-dev libxt-dev libncurses5 ncurses-dev suckless-tools rsync pulsemixer sshfs compton -y
-	sudo dpkg -i light_1.2_amd64.deb
-    sudo pip3 install youtube-dl
-}
+# Copy dotfiles
+sudo cp ~/dotfiles/.config/config.h_e550 ~/dotfiles/.config/dwm/config.h
 
-compile_py3_vim() {
-    git clone https://github.com/vim/vim ~/vim
-    cd ~/vim
-    ./configure --enable-perlinterp --enable-python3interp --enable-rubyinterp --enable-cscope --enable-gui=auto --enable-gtk2-check --enable-gnome-check --with-features=huge --enable-multibyte --with-x --with-compiledby='xorpd' --with-python3-config-dir=/usr/lib/python3.4/config-3.4m-x86_64-linux-gnu --prefix=/opt/vim74
-    make && sudo make install
-    sudo ln -s /opt/vim74/bin/vim /usr/bin/vim-py3
-}
+sudo cp -R ~/dotfiles/.config/dwm ~/.config/
+sudo cp -R ~/dotfiles/.config/mpd ~/.config/
+sudo cp -R ~/dotfiles/.config/ncmpcpp ~/.config/
+sudo cp -R ~/dotfiles/.config/qutebrowser ~/.config/
+sudo cp -R ~/dotfiles/.config/ranger ~/.config/
+sudo cp -R ~/dotfiles/.config/st ~/.config/
+sudo cp -R ~/dotfiles/.fonts ~/
 
-install_vim_plug() {
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-}
-   
-enable_services() {
-    sudo systemctl enable mpd
-    sudo systemctl enable pulseaudio
-    sudo systemctl enable network-manager
-}
 
-update_grub2() {
-    sudo update-grub2
-}
+sudo cp ~/dotfiles/.bash_aliases ~/
+sudo cp ~/dotfiles/.bash_profile ~/
+sudo cp ~/dotfiles/.vimrc ~/
+sudo cp ~/dotfiles/.bashrc ~/
 
-rm_programs() {
-    sudo apt purge nano youtube-dl -y && sudo apt remove nano youtube-dl -y
-}
 
-compile_dwm_and_st() {
-	cd ~/.config/dwm && sudo make clean install
-	cd ~/.config/st && sudo make clean install
-}
 
-clean_up() {
-    sudo chown $USER:$USER -R /sys/class/backlight/intel_backlight/brightness
-}
+# echo data into files
+## .xinitrc
+echo "xsetxkbmap -option caps:escape &" > ~/.xinitrc
+echo "xset r rate 200 50 &" >> ~/.xinitrc
+echo "xinput disable 'AlpsPS/2 ALPS DualPoint TouchPad' &" >> ~/.xinitrc
+echo "dwmbar_e550.sh &" >> ~/.xinitrc
+echo "" >> ~/.xinitrc
+echo "exec dwm" >> ~/.xinitrc
 
-main() {
-    get_dotfiles
-    apply_ownership
-    mk_dirs
-	mv_files_and_dirs
-    apply_ownership
-	echo_data
-    rm_programs
-    get_programs
-    compile_py3_vim
-    install_vim_plug
-    enable_services
-    apply_ownership
-    update_grub2
-	compile_dwm_and_st
-    clean_up
+## inputrc
+echo "set show-mode-in-prompt on" >> /etc/inputrc
 
-	/home/$USER/scripts/convert_configs_for_device.py "laptop"
-}
+## nobeep.conf
+echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 
-main
+## pulseaudio.service
+echo "[Unit]" > /etc/systemd/system/pulseaudio.service
+echo "Description=PulseAudio Daemon" >> /etc/systemd/system/pulseaudio.service
+echo "" >> /etc/systemd/system/pulseaudio.service
+echo "[Install]" >> /etc/systemd/system/pulseaudio.service
+echo "WantedBy=multi-user.target" >> /etc/systemd/system/pulseaudio.service
+echo "" >> /etc/systemd/system/pulseaudio.service
+echo "[Service]" >> /etc/systemd/system/pulseaudio.service
+echo "Type=simple" >> /etc/systemd/system/pulseaudio.service
+echo "PrivateTmp=true" >> /etc/systemd/system/pulseaudio.service
+echo "ExecStart=/usr/bin/pulseaudio --system --realtime --disallow" >> /etc/systemd/system/pulseaudio.service
+
+
+
+# Install programs
+sudo pacman -Syu ranger feh xorg xorg-xinit xorg-xinput xorg-xset xorg-xsetroot vim lxappearance pulseaudio curl mpd mpc ncmpcpp firefox python3 python-pip mpv imagemagick irssi newsboat fuse cifs-utils zathura zathura-cb zathura-pdf-poppler rsync pulsemixer sshfs light dos2unix ntfs-3g -y
+sudo pip3 install youtube-dl
+
+
+
+git clone https://github.com/vim/vim ~/vim
+cd ~/vim
+./configure --enable-perlinterp --enable-python3interp --enable-rubyinterp --enable-cscope --enable-gui=auto --enable-gtk2-check --enable-gnome-check --with-features=huge --enable-multibyte --with-x --with-compiledby='xorpd' --with-python3-config-dir=/usr/lib/python3.4/config-3.4m-x86_64-linux-gnu --prefix=/opt/vim74
+make && sudo make install
+sudo ln -s /opt/vim74/bin/vim /usr/bin/vim-py3
+
+
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+
+sudo systemctl enable mpd
+sudo systemctl enable pulseaudio
+
+
+
+sudo apt purge nano youtube-dl -y && sudo apt remove nano youtube-dl -y
+
+sudo chown $USER:wheel -R ~
+
+
+
+cd ~/.config/dwm && sudo make clean install
+cd ~/.config/st && sudo make clean install
+
+
+
+sudo chown $USER:wheel -R /sys/class/backlight/intel_backlight/brightness
+
+
+
+/home/$USER/scripts/convert_configs_for_device.py "laptop"
