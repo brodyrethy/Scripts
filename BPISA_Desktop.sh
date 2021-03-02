91ca2732db007699795e978d3e5877d046e39674
Brody Rethy <brody@rethy.xyz>
2021-03-02 15:45:51 -0400

    updated, time to test it.

--- BPISA_Desktop.sh
+++ BPISA_Desktop.sh
@@ -53,16 +53,16 @@ URL="https://github.com/rethyxyz/dotfiles"
 echo ""; echo "## File/directory hierarchy setup"
 ## Check if dir exists (I didn't break into function as it takes more code to
 ## write one over this)
-if [ -d $PATH ]
+if [ -d $HOME/dotfiles ]
 then
-	rm -rf ~/dotfiles && echo ":: Removed dotfiles directory" || { sudo rm -rf ~/dotfiles; echo ":: Removed dotfiles directory (used sudo)"; }
+	/usr/bin/rm -rf $HOME/dotfiles && echo ":: Removed dotfiles directory" || { sudo rm -rf $HOME/dotfiles; echo ":: Removed dotfiles directory (used sudo)"; }
 fi
 
 ## Download dotfiles
-/usr/bin/git clone $URL ~/dotfiles > /dev/null 2>&1 && echo ":: Downloaded dotfiles from $URL" || { echo ":: Failed to download dotfiles (they are important to the script)"; echo " Check Internet connection and try again"; exit 1; }
+/usr/bin/git clone $URL $HOME/dotfiles > /dev/null 2>&1 && echo ":: Downloaded dotfiles from $URL" || { echo ":: Failed to download dotfiles (they are important to the script)"; echo " Check Internet connection and try again"; exit 1; }
 
 ## Set proper permissions
-/usr/bin/sudo chown $USER:wheel -R ~ > /dev/null 2>&1 && echo ":: Set ~/$USER to $USER:wheel ownership" || { echo ":: Couldn't set ~/$USER ownership to $USER:wheel, this could be a problem later on..."; echo "   Come back after you fix the issue."; exit 126; }
+/usr/bin/sudo /usr/bin/chown $USER:wheel -R $HOME > /dev/null 2>&1 && echo ":: Set $HOME/$USER to $USER:wheel ownership" || { echo ":: Couldn't set $HOME/$USER ownership to $USER:wheel, this could be a problem later on..."; echo "   Come back after you fix the issue."; exit 126; }
 
 # This is a bad idea, but it MAY work. Well, lets hope
 # it does. I'm not sure what else to try here, so I'll
@@ -72,41 +72,43 @@ fi
 # I'll display status text after each iteration.
 #
 ## Makes non-normal files (need root privileges).
-PATHS=(
+FILES=(
 "/etc/inputrc"
 "/etc/modprobe.d/nobeep.conf"
 "/etc/systemd/system/pulseaudio.service"
 )
-for PATH in ${PATHS[@]}
+for FILE in ${FILES[@]}
 do
-	# Make file is it doesn't exist
-	if [ ! -e $PATH ]
+	# Make file if it doesn't exist
+	if [ ! -e $FILE ]
 	then
 		# If command fails, try sudo
-		{ /usr/bin/touch $PATH > /dev/null 2>&1 || /usr/bin/sudo /usr/bin/touch $PATH > /dev/null 2>&1 } && echo ":: Created $PATH"
+		{ /usr/bin/touch $FILE > /dev/null 2>&1 || /usr/bin/sudo /usr/bin/touch $FILE > /dev/null 2>&1 } && echo ":: Created $FILE"
 	fi
-	/usr/bin/sudo chmod 777 $PATH && echo "" # rwx for everyone!
-	/usr/bin/sudo chown -R $USER:wheel $PATH && echo ":: chown $USER:wheel successful" || echo ":: chown $USER:wheel failed" # change owner and group
+	/usr/bin/sudo chmod 777 $FILE && echo "" # rwx for everyone!
+	/usr/bin/sudo chown -R $USER:wheel $FILE && echo ":: chown $USER:wheel successful" || echo ":: chown $USER:wheel failed" # change owner and group
 done
 
+unset FILES
 ## Make dirs
-PATHS=(
-"~/.Trash/files"
-"~/.config/mpd/playlists"
-"~/.fonts"
-"~/.vim/undodir"
-"~/500GigDrive{0,1,2,3}" # TODO: I may have to split this
+FILES=(
+"$HOME/.Trash/files"
+"$HOME/.config/mpd/playlists"
+"$HOME/.fonts"
+"$HOME/.vim/undodir"
+"$HOME/500GigDrive{0,1,2,3}" # TODO: I may have to split this
 )
-for PATH in ${PATHS[@]}
+for FILE in ${FILES[@]}
 do
-	/usr/bin/sudo /usr/bin/mkdir -p $PATH && echo ":: Made $PATH" || ":: Failed to make $PATH"
+	/usr/bin/sudo /usr/bin/mkdir -p $FILE && echo ":: Made $FILE" || ":: Failed to make $FILE"
 done
 
 ## copy dwm's config.h
-/usr/bin/sudo /usr/bin/cp ~/dotfiles/.config/config.h_desktop ~/dotfiles/.config/dwm/config.h
+/usr/bin/sudo /usr/bin/cp $HOME/dotfiles/.config/config.h_desktop $HOME/dotfiles/.config/dwm/config.h
 
+unset FILES
 ## Copy from dotfiles directory
-PATHS=(
+FILES=(
 ".config/dunst"
 ".config/dwm"
 ".config/mpd"
@@ -122,51 +124,52 @@ PATHS=(
 ".bashrc"
 ".vim/macros.vim"
 )
-for PATH in $PATHS
+for FILE in ${FILES[@]}
 do
-	/usr/bin/sudo /usr/bin/cp -R $PATH && echo ":: Copied ~/dotfiles/$PATH to ~/$PATH" || echo ":: Failed to copy ~/dotfiles/$PATH to ~/$PATH"
+	/usr/bin/sudo /usr/bin/cp -R $FILE && echo ":: Copied $HOME/dotfiles/$FILE to $HOME/$FILE" || echo ":: Failed to copy $HOME/dotfiles/$FILE to $HOME/$FILE"
 done
 
+unset FILES
 ## Make symlinks
-PATHS=("music"
+FILES=("music"
 "vids"
 "docs"
 "notes"
 "pix"
 "repos")
-for PATH in ${PATHS[@]}
+for FILE in ${FILES[@]}
 do
-	/usr/bin/ln -sf $PATH && echo ":: Made symlink at $PATH" || echo ":: Couldn't make symlink at $PATH"
+	/usr/bin/ln -sf $FILE && echo ":: Made symlink at $FILE" || echo ":: Couldn't make symlink at $FILE"
 done
 
 ## Echo data into files. It's easier than having multiple files in my dotilfes repo.
 #
 ### For .xinitrc
-echo "xinput --set-prop 12 'libinput Accel Speed' -1 &" > ~/.xinitrc
-echo "setxkbmap -option caps:escape &" >> ~/.xinitrc
-echo "xset r rate 200 50 &" >> ~/.xinitrc
-echo "xinput disable 'AlpsPS/2 ALPS DualPoint TouchPad' &" >> ~/.xinitrc
-echo "dwmbar_desktop.sh &" >> ~/.xinitrc
-echo "dunst &" >> ~/.xinitrc
-echo "picom &" >> ~/.xinitrc
-echo "" >> ~/.xinitrc
-echo "exec dwm" >> ~/.xinitrc
+echo "xinput --set-prop 12 'libinput Accel Speed' -1 &" > $HOME/.xinitrc
+echo "setxkbmap -option caps:escape &" >> $HOME/.xinitrc
+echo "xset r rate 200 50 &" >> $HOME/.xinitrc
+echo "xinput disable 'AlpsPS/2 ALPS DualPoint TouchPad' &" >> $HOME/.xinitrc
+echo "dwmbar_desktop.sh &" >> $HOME/.xinitrc
+echo "dunst &" >> $HOME/.xinitrc
+echo "picom &" >> $HOME/.xinitrc
+echo "" >> $HOME/.xinitrc
+echo "exec dwm" >> $HOME/.xinitrc
 ### For inputrc
 echo "set show-mode-in-prompt on" >> /etc/inputrc
 ### For nobeep.conf
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 ### For pulseaudio.service
-echo "[Unit]" > ~/dotfiles/pulseaudio.service
-echo "Description=PulseAudio Daemon" >> ~/dotfiles/pulseaudio.service
-echo "" >> ~/dotfiles/pulseaudio.service
-echo "[Install]" >> ~/dotfiles/pulseaudio.service
-echo "WantedBy=multi-user.target" >> ~/dotfiles/pulseaudio.service
-echo "" >> ~/dotfiles/pulseaudio.service
-echo "[Service]" >> ~/dotfiles/pulseaudio.service
-echo "Type=simple" >> ~/dotfiles/pulseaudio.service
-echo "PrivateTmp=true" >> ~/dotfiles/pulseaudio.service
-echo "ExecStart=/usr/bin/pulseaudio --system --realtime --disallow" >> ~/dotfiles/pulseaudio.service
-mv ~/dotfiles/pulseaudio.service /etc/systemd/system/pulseaudio.service
+echo "[Unit]" > $HOME/dotfiles/pulseaudio.service
+echo "Description=PulseAudio Daemon" >> $HOME/dotfiles/pulseaudio.service
+echo "" >> $HOME/dotfiles/pulseaudio.service
+echo "[Install]" >> $HOME/dotfiles/pulseaudio.service
+echo "WantedBy=multi-user.target" >> $HOME/dotfiles/pulseaudio.service
+echo "" >> $HOME/dotfiles/pulseaudio.service
+echo "[Service]" >> $HOME/dotfiles/pulseaudio.service
+echo "Type=simple" >> $HOME/dotfiles/pulseaudio.service
+echo "PrivateTmp=true" >> $HOME/dotfiles/pulseaudio.service
+echo "ExecStart=/usr/bin/pulseaudio --system --realtime --disallow" >> $HOME/dotfiles/pulseaudio.service
+/usr/bin/mv $HOME/dotfiles/pulseaudio.service /etc/systemd/system/pulseaudio.service
 
 
 
@@ -192,8 +195,8 @@ dunst libnotify ranger scrot picard -y
 #
 # The newest version is needed for image previews with st. Ueberzug doesn't work with the Arch repo version.
 {
-/usr/bin/git clone https://github.com/ranger/ranger ~/ranger
-cd ~/ranger
+/usr/bin/git clone https://github.com/ranger/ranger $HOME/ranger
+cd $HOME/ranger
 /usr/bin/sudo make clean install
 } > /dev/null 2>&1 && echo ":: Compiled and installed newest ranger" || echo ":: Failed to install newest ranger"
 
@@ -201,15 +204,15 @@ cd ~/ranger
 #
 # Needed for LaTeX live preview, an plugin for vim.
 {
-/usr/bin/git clone https://github.com/vim/vim ~/vim
-cd ~/vim
+/usr/bin/git clone https://github.com/vim/vim $HOME/vim
+cd $HOME/vim
 ./configure --enable-perlinterp --enable-python3interp --enable-rubyinterp --enable-cscope --enable-gui=auto --enable-gtk2-check --enable-gnome-check --with-features=huge --enable-multibyte --with-x --with-compiledby='xorpd' --with-python3-config-dir=/usr/lib/python3.4/config-3.4m-x86_64-linux-gnu --prefix=/opt/vim74
 make && /usr/bin/sudo make install
 /usr/bin/sudo ln -s /opt/vim74/bin/vim /usr/bin/vim-py3
 } > /dev/null 2>&1 && echo ":: Compiled and installed vim-py3" || echo ":: Failed to install newest vim-py3"
 
 ## vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1 && echo ":: Installing vim-plug" || echo ":: Failed to install vim-plug"
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1 && echo ":: Installing vim-plug" || echo ":: Failed to install vim-plug"
 
 
 
@@ -225,13 +228,13 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 
 
 ## Set proper permissions
-/usr/bin/sudo chown $USER:wheel -R ~ > /dev/null 2>&1 && echo ":: Set ~/$USER to $USER:wheel ownership" || echo ":: Couldn't set ~/$USER ownership to $USER:wheel"
+/usr/bin/sudo chown $USER:wheel -R $HOME > /dev/null 2>&1 && echo ":: Set $HOME/$USER to $USER:wheel ownership" || echo ":: Couldn't set $HOME/$USER ownership to $USER:wheel"
 
 
 
 # Compile suckless programs
-cd ~/.config/dwm && /usr/bin/sudo make clean install > /dev/null 2>&1 && echo ":: Compile dwm successful" || echo ":: Failed to compile dwm" 
-cd ~/.config/st && /usr/bin/sudo make clean install > /dev/null 2>&1 && echo ":: Compile st successful" || echo ":: Failed to compile st"
+cd $HOME/.config/dwm && /usr/bin/sudo make clean install > /dev/null 2>&1 && echo ":: Compile dwm successful" || echo ":: Failed to compile dwm" 
+cd $HOME/.config/st && /usr/bin/sudo make clean install > /dev/null 2>&1 && echo ":: Compile st successful" || echo ":: Failed to compile st"
 
 
 
