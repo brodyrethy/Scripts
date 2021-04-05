c0c832899259207fdeb84f2b502cde8ae15ba870
Brody Rethy <brodyrethy@gmail.com>
2021-04-05 12:13:28 -0300

    pushing

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 #
-#	By: Brody Rethy
-#	Website: https://rethy.xyz 
+## By: Brody Rethy
+## Website: https://rethy.xyz
 #
-#	Name: backup_dotfiles.sh
-#	Version: 1.0
+## Name: backup_dotfiles.sh
+## Version: 1.0
 #
-#	Summary:
-#	Used for backing up system dotfiles to 
-#	dotfiles directory (~/repos/dotfiles).
+## Summary:
+## Used for backing up system dotfiles to
+## dotfiles directory (~/repos/dotfiles).
 #
-#	Requires one argument defining the device,
-#	being a desktop or laptop system, as some
-#	configs differ between system to system.
-#
-# TODO: Make dotfiles path variable
-#
+## Requires one argument defining the device,
+## being a desktop or laptop system, as some
+## configs differ between system to system.
 
+# file list goes here (taken from the perspective of user $HOME directory)
 FILES=(
 ".bash_aliases"
 ".bash_profile"
@@ -31,25 +29,27 @@ FILES=(
 ".config/ranger/rc.conf"
 ".config/ranger/rifle.conf"
 ".newsboat/config"
-".vim/keybindings.vim"
+".vim/abbreviation.vim"
+".vim/leader.vim"
+".vim/macro.vim"
+".vim/remap.vim"
 ".vimrc"
 )
 
 for FILE in ${FILES[@]}
 do
-	/usr/bin/diff "$HOME/repos/dotfiles/$FILE" "$HOME/$FILE" 
+	/usr/bin/diff -n "$HOME/repos/dotfiles/$FILE" "$HOME/$FILE"
 
 	echo "Are you sure you want to replace $FILE file found in ~/repos/dotfiles? (y/n)"
 	read CHOICE
 
-	if [ "$CHOICE" = "y" ] || [ "$CHOICE" = "Y" ]
-	then
-		/usr/bin/cp -R "$HOME/$FILE" "$HOME/repos/dotfiles/$FILE"
-		clear
-	else
-		clear
-		echo ":: Passed on $FILE"
-	fi
+	case "$CHOICE" in
+		y | Y) /usr/bin/cp -R "$HOME/$FILE" "$HOME/repos/dotfiles/$FILE"; clear ;;
+		*) clear; echo ":: Passed on $FILE" ;;
+	esac
+
 done
+
+echo "All done!"
 
 exit 0
